e549334da79ffd9be69c0ce38e722cabca6cb5a3
yue ye <1650958692@qq.com>
2024-09-22 21:55:40 +0800

    color

--- web.py
+++ web.py
@@ -3,13 +3,22 @@ import streamlit as st
 
 
 st.header('这里是刷题网站的主页')
-chapter = st.selectbox("数学科目选择", ('高数', '线代', '概率论'))
-if chapter == '高数':
-    selection = st.selectbox('章节选择', ('第一节', '第二节'))
-    st.write(f'你选择的科目是{chapter},选择的章节是{selection}')
-if chapter == '线代':
-    selection = st.selectbox('章节选择', ('第一节', '第二节'))
-    st.write(f'你选择的科目是{chapter},选择的章节是{selection}')
-if chapter == '概率论':
-    selection = st.selectbox('章节选择', ('第一节', '第二节'))
-    st.write(f'你选择的科目是{chapter},选择的章节是{selection}')
+with st.form('数学章节选择'):
+    st.subheader('**选择你的数学科目和章节**')
+
+    # Input widgets
+    chapter = st.selectbox("数学科目选择", ('高数', '线代', '概率论'))
+    selection = st.slider('章节选择', min_value=1, max_value=36, step=1)
+    type = st.selectbox('题目类型', ['错题', '例题'])
+    # Every form must have a submit button
+    submitted = st.form_submit_button('提交你的选择')
+
+if submitted:
+    st.markdown(f'''
+        你选择的题目类型是
+        - 课程类型: `{chapter}`
+        - 章节: `{selection}`
+        - 类型: `{type}`
+        ''')
+else:
+    st.write('☝️ Place your order!')
