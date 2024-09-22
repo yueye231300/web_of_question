75dbcef04c4bdc86639ce358c22aa4a7f59077f9
yue ye <1650958692@qq.com>
2024-09-22 21:45:59 +0800

    color

--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,4 @@
-# package setting
 import streamlit as st
-
 
 st.header('这里是刷题网站的主页')
 chapter = st.selectbox("数学科目选择", ('高数', '线代', '概率论'))
@@ -13,3 +11,10 @@ if chapter == '线代':
 if chapter == '概率论':
     selection = st.selectbox('章节选择', ('第一节', '第二节'))
     st.write(f'你选择的科目是{chapter},选择的章节是{selection}')
+
+
+
+
+
+
+
