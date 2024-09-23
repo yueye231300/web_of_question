a7675b795acc030716f79b716fbcd5200adb3a93
yue ye <1650958692@qq.com>
2024-09-23 19:17:24 +0800

    try again

--- web.py
+++ web.py
@@ -1,16 +1,54 @@
 # package setting
 import streamlit as st
 
+
+
+def login():
+    st.header('选择你自己需要刷的题目')
+    chapter = st.selectbox("数学科目选择", ('高数', '线代', '概率论', None), key='chapter')
+    selection = st.slider('章节选择', min_value=1, max_value=36, step=1, key='selection',)
+    type = st.selectbox('题目类型', ['错题', '例题', None], key='type')
+    if st.button("提交选择"):
+        st.session_state.chapter = chapter
+        st.session_state.selection = selection
+        st.session_state.type = type
+        st.rerun()
+
+def logout():
+    st.session_state.chapter = None
+    st.session_state.selection = 1
+    st.session_state.type = None
+    st.rerun()
+
 if "chapter" in st.session_state:
     st.session_state.chapter = st.session_state.chapter
 if "selection" in st.session_state:
     st.session_state.selection = st.session_state.selection
 if "type" in st.session_state:
     st.session_state.type = st.session_state.type
-st.header('这里是刷题网站的主页')
 
-chapter = st.selectbox("数学科目选择", ('高数', '线代', '概率论'), key='chapter')
-selection = st.slider('章节选择', min_value=1, max_value=36, step=1, key='selection')
-type = st.selectbox('题目类型', ['错题', '例题'], key='type')
 
-st.Page("pages/home.py",title="home")
+logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
+home = st.Page(
+    "pages/home.py",
+    title="home",
+    icon=":material/help:",
+)
+test = st.Page("pages/test.py",
+    title='test',
+    icon=":material/help:"
+)
+account_pages = [logout_page]
+# 导航页面
+st.title("Request manager")
+page_dict = {}
+if st.session_state.type == '错题':
+    page_dict["Request"] = home
+if st.session_state.type == '例题':
+    page_dict["Respond"] = test
+
+if len(page_dict) > 0:
+    pg = st.navigation({"Account": account_pages} | page_dict)
+
+else:
+    pg = st.navigation([st.Page(login)])
