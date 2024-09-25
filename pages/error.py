cc8af09233f1516a7252b0067f93c7553c0262dc
yue ye <1650958692@qq.com>
2024-09-25 19:41:45 +0800

    try twice

--- pages/error.py
+++ pages/error.py
@@ -8,7 +8,7 @@ if "type" in st.session_state:
     st.session_state.type = st.session_state.type
 
 st.markdown('**你输入的文字是**')
-st.write(st.session_state.selection)
+
 
 
 
--- pages/test.py
+++ pages/test.py
@@ -8,4 +8,3 @@ if "type" in st.session_state:
     st.session_state.type = st.session_state.type
 
 st.markdown('**你输入的文字是**')
-st.write(st.session_state.selection)
