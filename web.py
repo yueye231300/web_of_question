ba3a649aaf5ca61f4e73e410b33693e7fcc5044d
yue ye <1650958692@qq.com>
2024-09-25 19:41:07 +0800

    try twice

--- web.py
+++ web.py
@@ -40,10 +40,9 @@ request_pages = [test, error]
 st.title("Request manager")
 
 page_dict = {}
-if st.session_state.type in ["错题"]:
-    page_dict["练习例题"] = test
-if st.session_state.type in ["例题"]:
-    page_dict["练习错题"] = error
+if st.session_state.type in ["错题", "例题"]:
+    page_dict["练习例题"] = request_pages
+
 
 
 if len(page_dict) > 0:
